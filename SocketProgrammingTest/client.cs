d54f10da6950e68be83e75b9170046b345b81569
AbsorbedBarrel <47505192+AbsorbedBarrel@users.noreply.github.com>
2020-11-21 10:57:36 -0500

    Add files via upload

---
+++ SocketProgrammingTest/client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client {
+    class Program
+        {
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+
+        static void ExecuteClient() 
+        {
+            try
+              {
+
+                //Get the IP Address and Select a Remote End Point
+                IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress address = host.AddressList[0];
+                IPEndPoint endPoint= new IPEndPoint(address, 12345);
+                //Open the Socket
+                Socket sender = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    sender.Connect(endPoint);
+                    Console.WriteLine("Connected to Socket: {0}", sender.RemoteEndPoint.ToString());
+
+                    byte[] sentMessage = Encoding.ASCII.GetBytes("Test Message<End>");
+                    int sentBytes = sender.Send(sentMessage);
+
+                    byte[] recievedMessage = new byte[1024];
+
+                    int recievedBytes = sender.Receive(recievedMessage);
+                    Console.WriteLine("Message from Server -> {0}",
+                          Encoding.ASCII.GetString(recievedMessage,
+                                                     0, recievedBytes));
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane) 
+                {
+                    Console.WriteLine(ane);
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine(se);
+                }
+                catch (Exception ane)
+                {
+                    Console.WriteLine(ane);
+                }
+            }
+              catch (Exception e) 
+              {
+                 Console.WriteLine(e);
+              }
+            }
+        }
+    }
+    
+
---
+++ SocketProgrammingTest/server.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+
+        static void ExecuteServer()
+        {
+            //Get the IP Address and Select a Remote End Point
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress address = host.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(address, 12345);
+
+            Socket listener = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                //Associate an End Point with the socket
+                listener.Bind(endPoint);
+                listener.Listen(10);
+
+                while (true) 
+                {
+                    Console.WriteLine("Waiting for client connection ...");
+
+                    //Set the senderSocket to the socket that connects
+                    Socket senderSocket = listener.Accept();
+
+                    //Setup a buffer for incoming data
+                    byte[] bytes = new byte[1024];
+                    string data = null;
+
+                    //Go through all of the messages until "<End>" appears
+                    while (true) 
+                    {
+                        int numByte = senderSocket.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes,
+                                                   0, numByte);
+                        if (data.IndexOf("<End>") > -1)
+                            break;
+                    }
+
+                    //Send message back to client
+                    Console.WriteLine("Text Recieved -> {0}", data);
+                    byte[] message = Encoding.ASCII.GetBytes("Server Ping");
+
+                    senderSocket.Send(message);
+
+                    senderSocket.Shutdown(SocketShutdown.Both);
+                    senderSocket.Close();
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
